4ddfd922efe5ee9b63a4444620e719aa0f6158da
Waihon Yew <yewwaihon@gmail.com>
2021-06-06 05:49:36 +0800

    Initial Commit

---
+++ Multiplication/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Multiplication
+//
+//  Created by Waihon Yew on 06/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
